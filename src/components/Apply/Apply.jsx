17148d02595b665eca3973bf240d8aaa3e2a5a63
Kashmir Shrestha <cthakashmir999@gmail.com>
2026-02-23 23:51:36 +0545

    Tensor v3.7 --registration form closed

--- src/components/Apply/Apply.jsx
+++ src/components/Apply/Apply.jsx
@@ -8,7 +8,7 @@ export default function Apply() {
   useEffect(() => {
     window.scrollTo(0, 0)
   }, [])
-  const applicationClosed = false;
+  const applicationClosed = true;
   return (
     <>
       {applicationClosed ? (
